6ea1c0a90d9a6e8786297a7405b3f327735fd9f1
Edgar Montano <em2532@nyu.edu>
2019-07-06 14:10:11 -0400

    added static build

---
+++ precache-manifest.a52fa94cd460529cb33ea5a2d76ccace.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad3251815ae5c008c3bd39c745ce6169",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f707601f67cf9d8a8d7",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "1b797d5fb3e7f984e260",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "7f707601f67cf9d8a8d7",
+    "url": "/static/js/2.8e81696d.chunk.js"
+  },
+  {
+    "revision": "1b797d5fb3e7f984e260",
+    "url": "/static/js/main.bb11bc9b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
